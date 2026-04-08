9fc76c3069812316bc2f3223d814f929032492cb
Homeph5319 <166377352+Homeph5319@users.noreply.github.com>
2026-04-08 13:30:52 +0800

    Update build-gcc.sh

--- build-gcc.sh
+++ build-gcc.sh
@@ -20,10 +20,8 @@ done
 
 # TODO: Better target handling
 case "${arch}" in
-  "arm") TARGET="arm-eabi" ;;
-  "arm64") TARGET="aarch64-elf" ;;
+  "armgnu") TARGET="arm-linux-gnueabi" ;;
   "arm64gnu") TARGET="aarch64-linux-gnu" ;;
-  "x86") TARGET="x86_64-elf" ;;
 esac
 
 export WORK_DIR="$PWD"
